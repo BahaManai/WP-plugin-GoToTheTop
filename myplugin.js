44a845c02c438b205798fedf9fa302737e573b81
Baha Manai <70824369+BahaManai@users.noreply.github.com>
2024-08-27 21:51:56 +0100

    JS deleted

--- myplugin.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-    $("#GoToTheTop1, #GoToTheTop2, #GoToTheTop3, #GoToTheTop4").on('click touchstart', function(e) {
-      e.preventDefault();
-      $("body,html").animate({ scrollTop: 0 }, 500);
-      return false;
-    });
-  });
-  
